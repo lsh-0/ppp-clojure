c458ecc70966e409857bb23e1568311d32aa6ad1
Luke <lsh-0@users.noreply.github.com>
2022-08-09 09:15:58 +0930

    shifts api routes in http-server under 'api' adds default 404 for any route not found adds a landing page route for /api

--- bases/http-server/src/ppp/http_server/core.clj
+++ bases/http-server/src/ppp/http_server/core.clj
@@ -1,7 +1,7 @@
 (ns ppp.http-server.core
   (:require
-   [compojure.core :refer [defroutes GET]]
-   [compojure.route :as route]
+   [compojure.core :refer [GET]]
+   [compojure.route]
    [ring.adapter.jetty :as jetty]
    [ppp.lax.interface :as articles]
    [ppp.api-raml.interface :as api-raml]
@@ -56,7 +56,7 @@
 
 (defn acceptable-content-types-middleware
   "we know the types of content supported because of the api-raml.
-  any requests for content (or specific versions of content) that are not supported are not made."
+  any requests for content (or specific versions of content) that are not supported are not propagated."
   [handler]
   (fn [request]
     (let [accept-header (-> request :headers (get "accept"))
@@ -76,27 +76,38 @@
         resp (apply f (conj (vec rest) kwargs))]
     (if-let [proxy-resp (:http-resp resp)]
       proxy-resp
-        
+
       {:status 200
        :body (:content resp)})))
 
 ;;
 
-(defroutes api-routes
-  (GET "/articles" [page per-page order api-key :as {content-type-list :content-type-list}]
-       (api-response-handler articles/article-list content-type-list page per-page order api-key))
+(defn landing-page-handler
+  [req]
+  {:status 200
+   :body "api landing page"})
 
-  (GET "/articles/:id" [id api-key :as {content-type-list :content-type-list}]
-       (api-response-handler articles/article id content-type-list api-key))
-  
-  (GET "/articles/:id/related" [id api-key :as {content-type-list :content-type-list}]
-       (api-response-handler articles/related-article-list id content-type-list api-key))
+(def routes
+  (compojure.core/routes
 
-  (GET "/articles/:id/versions" [id api-key :as {content-type-list :content-type-list}]
-       (api-response-handler articles/article-version-list id content-type-list api-key))
+   (GET "/api" [] landing-page-handler)
+   
+   (GET "/api/articles" [page per-page order api-key :as {content-type-list :content-type-list}]
+        (api-response-handler articles/article-list content-type-list page per-page order api-key))
 
-  (GET "/articles/:id/versions/:version" [id version api-key :as {content-type-list :content-type-list}]
-       (api-response-handler articles/article-version id version content-type-list api-key)))
+   (GET "/api/articles/:id" [id api-key :as {content-type-list :content-type-list}]
+        (api-response-handler articles/article id content-type-list api-key))
+   
+   (GET "/api/articles/:id/related" [id api-key :as {content-type-list :content-type-list}]
+        (api-response-handler articles/related-article-list id content-type-list api-key))
+
+   (GET "/api/articles/:id/versions" [id api-key :as {content-type-list :content-type-list}]
+        (api-response-handler articles/article-version-list id content-type-list api-key))
+
+   (GET "/api/articles/:id/versions/:version" [id version api-key :as {content-type-list :content-type-list}]
+        (api-response-handler articles/article-version id version content-type-list api-key))
+
+   (compojure.route/not-found "404 Not Found.")))
 
 (defn run-server
   "starts a http web server on an unprivileged port, does not block thread by default.
@@ -109,17 +120,17 @@
                    :max-threads 50
                    :min-threads 8}
         opts (merge repl-opts opts)
-        routes (-> api-routes
-                   (wrap-defaults api-defaults)
-                   (acceptable-content-types-middleware))
+        routing (-> routes
+                    (wrap-defaults api-defaults)
+                    (acceptable-content-types-middleware))
         ]
     (println "opts" opts)
-    (jetty/run-jetty routes opts)))
+    (jetty/run-jetty routing opts)))
 
 (defn -main
   "starts a http web server on port 80"
   [& args]
-  ;; make some options more visible. full set here:
+  ;; full set of options:
   ;; - https://github.com/ring-clojure/ring/blob/1.9.4/ring-jetty-adapter/src/ring/adapter/jetty.clj
   (let [opts {;; blocks thread until server ends, rendering a REPL unusable.
               :join? true}]
